1a01fbdbe44a5bd8eea7a5050066bdd14acd943f
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2022-07-07 01:35:40 -0300

    LeftSide

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React from "react";
 
-// import { Container } from './styles';
+import { Container, Main, LeftSide, RightSide } from "./styles";
+import ProfileData from './../../components/ProfileData';
 
 const Profile: React.FC = () => {
-  return <div />;
-}
+  return (
+    <Container>
+      <Main>
+        <LeftSide>
+          <ProfileData 
+          username="@lesada"
+          name="Leslie"
+          avatarUrl="https://avatars.githubusercontent.com/u/68572174?v=4"
+          followers={33}
+          following={17}
+          company={undefined}
+          location={"Brasil"}
+          email={undefined}
+          blog={undefined}
+          />
+        </LeftSide>
+        <RightSide></RightSide>
+      </Main>
+    </Container>
+  );
+};
 
 export default Profile;
