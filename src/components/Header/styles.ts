af29644553a514b05858767b94f7adc8c19f51b8
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2022-07-09 22:51:36 -0300

    Dark Theme

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -12,6 +12,12 @@ export const GithubLogo = styled(FaGithub)`
   width: 32px;
   height: 32px;
   flex-shrink: 0;
+
+  cursor: pointer;
+
+  &:hover {
+    opacity: 0.8;
+  }
 `;
 export const SearchForm = styled.form`
   padding-left: 16px;
