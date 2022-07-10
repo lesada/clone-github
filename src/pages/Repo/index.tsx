b29a7e074b73316a5035f31ed10571faf7aa45f0
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2022-07-09 21:31:49 -0300

    Repo Route

--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,9 +1,50 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
-// import { Container } from './styles';
+import {
+  Container,
+  Breadcrumb,
+  RepoIcon,
+  Stats,
+  StarIcon,
+  ForkIcon,
+  LinkButton,
+  GithubIcon,
+} from "./styles";
 
 const Repo: React.FC = () => {
-  return <div />;
-}
+  return (
+    <Container>
+      <Breadcrumb>
+        <RepoIcon />
+        <Link className={"username"} to={"/lesada"}>
+          lesada
+        </Link>
+        <span>/</span>
+        <Link className={"reponame"} to={"/lesada/twitter-clone"} />
+      </Breadcrumb>
+
+      <p>Twitter clone</p>
+
+      <Stats>
+        <li>
+          <StarIcon />
+          <b>9</b>
+          <span>stars</span>
+        </li>
+        <li>
+          <ForkIcon />
+          <b>0</b>
+          <span>forks</span>
+        </li>
+      </Stats>
+
+      <LinkButton href="https://github.com/lesada/twitter-clone">
+        <GithubIcon />
+        <span>View on github</span>
+      </LinkButton>
+    </Container>
+  );
+};
 
 export default Repo;
