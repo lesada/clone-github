57d5479942fb644bb522943384a650353ee32a8d
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2022-07-09 22:28:25 -0300

    Search from API

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,32 +2,32 @@ import styled from "styled-components";
 import { FaGithub } from "react-icons/fa";
 
 export const Container = styled.div`
-    display: flex;
-    align-items: center;
-    background: var(--header);
-    padding: 11px 16px;
+  display: flex;
+  align-items: center;
+  background: var(--header);
+  padding: 11px 16px;
 `;
 export const GithubLogo = styled(FaGithub)`
-    fill: var(--logo);
-    width: 32px;
-    height: 32px;
-    flex-shrink: 0;
+  fill: var(--logo);
+  width: 32px;
+  height: 32px;
+  flex-shrink: 0;
 `;
-export const SearchForm = styled.div`
-    padding-left: 16px;
+export const SearchForm = styled.form`
+  padding-left: 16px;
+  width: 100%;
+
+  input {
+    background: var(--search);
+    border-radius: 6px;
+    outline: none;
+    padding: 7px 12px;
     width: 100%;
 
-    input {
-        background: var(--search);
-        border-radius: 6px;
-        outline: none;
-        padding: 7px 12px;
-        width: 100%;
-
-        &:focus {
-            width: 318px;
-        }
-
-        transition: width 0.2s ease-out, color 0.2s ease-out;
+    &:focus {
+      width: 318px;
     }
+
+    transition: width 0.2s ease-out, color 0.2s ease-out;
+  }
 `;
